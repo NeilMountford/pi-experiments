7f699d50a44f4912170025ac380b4cfe153d33ab
Neil Mountford <2104393+OmNomRarg@users.noreply.github.com>
2019-07-07 17:48:30 +0100

    Change the state of the LED when the button is pressed for more than 50ms, exit if button is held for 2s

--- ButtonLamp/Program.cs
+++ ButtonLamp/Program.cs
@@ -12,23 +12,62 @@ namespace ButtonLamp
                 var ledPin = 11;
                 var buttonPin = 12;
 
+                var ledState = PinValue.Low;
+                var buttonState = PinValue.High;
+                var lastButtonState = PinValue.High;
+                PinValue reading;
+                var lastChangeTime = DateTime.UtcNow;
+                var captureTime = TimeSpan.FromMilliseconds(50);
+                var exitTime = TimeSpan.FromSeconds(2);
+
                 gpioController.OpenPin(ledPin, PinMode.Output);
                 gpioController.OpenPin(buttonPin, PinMode.InputPullUp);
 
-                var exitAt = DateTime.UtcNow.AddSeconds(20);
-
                 try
                 {
-                    while (DateTime.UtcNow < exitAt)
+                    while (true)
                     {
-                        if (gpioController.Read(buttonPin) == PinValue.Low)
+                        reading = gpioController.Read(buttonPin);
+
+                        if(reading != lastButtonState)
                         {
-                            gpioController.Write(ledPin, PinValue.High);
+                            lastChangeTime = DateTime.UtcNow;
                         }
-                        else
+                        else if (reading == PinValue.Low && DateTime.UtcNow - lastChangeTime > exitTime)
                         {
-                            gpioController.Write(ledPin, PinValue.Low);
+                            System.Console.WriteLine("Pressed and held to exit");
+                            break;
                         }
+
+                        if(DateTime.UtcNow - lastChangeTime > captureTime)
+                        {
+                            if(reading != buttonState)
+                            {
+                                buttonState = reading;
+
+                                if(buttonState == PinValue.Low)
+                                {
+                                    System.Console.WriteLine("Button pressed");
+                                    ledState = ledState == PinValue.Low ? PinValue.High : PinValue.Low;
+
+                                    if(ledState == PinValue.High)
+                                    {
+                                        System.Console.WriteLine("Turning on LED");
+                                    }
+                                    else
+                                    {
+                                        System.Console.WriteLine("Turning off LED");
+                                    }
+                                }
+                                else
+                                {
+                                    System.Console.WriteLine("Button released");
+                                }
+                            }
+                        }
+
+                        gpioController.Write(ledPin, ledState);
+                        lastButtonState = reading;
                     }
                 }
                 finally
