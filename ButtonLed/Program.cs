201e184e4dcf03b1168e20c4d6f85c13f808a6ca
Neil Mountford <2104393+OmNomRarg@users.noreply.github.com>
2019-07-07 13:26:30 +0100

    Initial commit

---
+++ ButtonLed/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace ButtonLed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(var gpioController = new GpioController(PinNumberingScheme.Board))
+            {
+                var ledPin = 11;
+                var buttonPin = 12;
+
+                gpioController.OpenPin(ledPin, PinMode.Output);
+                gpioController.OpenPin(buttonPin, PinMode.InputPullUp);
+
+                var exitAt = DateTime.UtcNow.AddSeconds(20);
+
+                try
+                {
+                    while(DateTime.UtcNow < exitAt)
+                    {
+                        if(gpioController.Read(buttonPin) == PinValue.Low)
+                        {
+                            gpioController.Write(ledPin, PinValue.High);
+                        }
+                        else
+                        {
+                            gpioController.Write(ledPin, PinValue.Low);
+                        }
+                    }
+                }
+                finally
+                {
+                    gpioController.Write(ledPin, PinValue.Low);
+                    gpioController.ClosePin(ledPin);
+                    gpioController.ClosePin(buttonPin);
+                    System.Console.WriteLine("Closed pins");
+                }
+            }
+        }
+    }
+}
---
+++ LedBlink/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Threading;
+
+namespace LedBlink
+{
+    class Program
+    {
+        private const int NumberOfTimesToBlink = 4;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Blinking the LED {NumberOfTimesToBlink} times");
+
+            using(var gpioController = new GpioController(PinNumberingScheme.Board))
+            {
+                var pin = 11;
+
+                gpioController.OpenPin(pin, PinMode.Output);
+
+                try
+                {
+                    for (var i = 0; i < NumberOfTimesToBlink; i++)
+                    {
+                        gpioController.Write(pin, PinValue.High);
+                        Thread.Sleep(500);
+                        gpioController.Write(pin, PinValue.Low);
+                        Thread.Sleep(500);
+                    }
+                }
+                finally
+                {
+                    gpioController.ClosePin(pin);
+                }
+            }
+        }
+    }
+}
