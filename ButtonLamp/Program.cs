78dec2e1c9e6ad6f8c0223206482b2b3845736d0
Neil Mountford <2104393+OmNomRarg@users.noreply.github.com>
2019-07-07 17:23:03 +0100

    Added basic project for a 'Lamp', starting with the same program as ButtonLed

---
+++ ButtonLamp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Device.Gpio;
+
+namespace ButtonLamp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var gpioController = new GpioController(PinNumberingScheme.Board))
+            {
+                var ledPin = 11;
+                var buttonPin = 12;
+
+                gpioController.OpenPin(ledPin, PinMode.Output);
+                gpioController.OpenPin(buttonPin, PinMode.InputPullUp);
+
+                var exitAt = DateTime.UtcNow.AddSeconds(20);
+
+                try
+                {
+                    while (DateTime.UtcNow < exitAt)
+                    {
+                        if (gpioController.Read(buttonPin) == PinValue.Low)
+                        {
+                            gpioController.Write(ledPin, PinValue.High);
+                        }
+                        else
+                        {
+                            gpioController.Write(ledPin, PinValue.Low);
+                        }
+                    }
+                }
+                finally
+                {
+                    gpioController.Write(ledPin, PinValue.Low);
+                    gpioController.ClosePin(ledPin);
+                    gpioController.ClosePin(buttonPin);
+                    System.Console.WriteLine("Closed pins");
+                }
+            }
+        }
+    }
+}
---
+++ ButtonLamp/publish.bat
@@ -0,0 +1,2 @@
+dotnet publish -r linux-arm
+rsync -razvp --chmod=Fu=rwx ./bin/Debug/netcoreapp2.2/linux-arm/publish/* pi@nompi4.local:/home/pi/development/ButtonLamp/
